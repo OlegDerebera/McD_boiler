0cfeb5fecf4205e4a2eb1617241c7103fdea552d
Oleg <Oleg@WIN-B7OF66KUMUC>
2023-06-16 16:31:06 +0300

    First commit

---
+++ components/lcd_ili9342c/include/ili9342c.h
@@ -0,0 +1,62 @@
+#ifndef MAIN_ILI9342C_H_
+#define MAIN_ILI9342C_H_
+//---------------------------------------------------------------------
+#include <unistd.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include <driver/spi_master.h>
+#include "../../build/config/sdkconfig.h"
+
+#include "fonts.h"
+//---------------------------------------------------------------------
+static uint16_t TFT9341_WIDTH;
+static uint16_t TFT9341_HEIGHT;
+static spi_device_handle_t spi;
+//---------------------------------------------------------------------
+#define swap(a,b) {int16_t t=a;a=b;b=t;}
+//---------------------------------------------------------------------
+#define TFT9341_MADCTL_MY  0x80
+#define TFT9341_MADCTL_MX  0x40
+#define TFT9341_MADCTL_MV  0x20
+#define TFT9341_MADCTL_ML  0x10
+#define TFT9341_MADCTL_RGB 0x00
+#define TFT9341_MADCTL_BGR 0x08
+#define TFT9341_MADCTL_MH  0x04
+#define TFT9341_BLACK   0x0000
+#define TFT9341_BLUE    0x001F
+#define TFT9341_RED     0xF800
+#define TFT9341_GREEN   0x07E0
+#define TFT9341_CYAN    0x07FF
+#define TFT9341_MAGENTA 0xF81F
+#define TFT9341_YELLOW  0xFFE0
+#define TFT9341_WHITE   0xFFFF
+#define CONFIG_PIN_NUM_MOSI 23
+#define CONFIG_PIN_NUM_MISO 25
+#define CONFIG_PIN_NUM_CLK 18
+#define CONFIG_PIN_NUM_CS 14
+#define CONFIG_PIN_NUM_DC 27
+#define CONFIG_PIN_NUM_BCKL 32
+#define CONFIG_PIN_NUM_RST 33
+//---------------------------------------------------------------------
+void TFT9341_FillScreen(spi_device_handle_t spi, uint16_t color);
+void TFT9341_FillRect(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color);
+void TFT9341_DrawPixel(spi_device_handle_t spi, int x, int y, uint16_t color);
+void TFT9341_DrawLine(spi_device_handle_t spi, uint16_t color, uint16_t x1, uint16_t y1,
+                      uint16_t x2, uint16_t y2);
+void TFT9341_DrawRect(spi_device_handle_t spi, uint16_t color, uint16_t x1, uint16_t y1,
+                      uint16_t x2, uint16_t y2);
+void TFT9341_DrawCircle(spi_device_handle_t spi, uint16_t x0, uint16_t y0, int r, uint16_t color);
+void TFT9341_SetTextColor(uint16_t color);
+void TFT9341_SetBackColor(uint16_t color);
+void TFT9341_SetFont(sFONT *pFonts);
+void TFT9341_DrawChar(spi_device_handle_t spi, uint16_t x, uint16_t y, uint8_t c);
+void TFT9341_String(spi_device_handle_t spi, uint16_t x,uint16_t y, char *str);
+void TFT9341_SetRotation(spi_device_handle_t spi, uint8_t r);
+void TFT9341_ini(spi_device_handle_t spi, uint16_t w_size, uint16_t h_size);
+//void TFT9341_spi_init(void);
+//void lcd_spi_pre_transfer_callback(spi_transaction_t *t);
+//---------------------------------------------------------------------
+#endif /* MAIN_ILI9342C_H_ */
---
+++ main/main.c
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "ds18b20.h"
+#include "ili9342c.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include <driver/spi_master.h>
+#include "esp_log.h"
+#include "esp_err.h"
+
+#define TEMP_BUS 22
+#define LED 2
+#define HIGH 1
+#define LOW 0
+#define digitalWrite gpio_set_level
+
+void lcd_spi_pre_transfer_callback(spi_transaction_t *t);
+void TFT9341_spi_init(void);
+//---------------------------------------------------------------------------
+static const char* TAG = "McD";
+uint8_t dt[9];
+float cTemp;
+char str[26];
+uint8_t rom[9];
+typedef struct {
+	uint16_t x1;
+	uint16_t x2;
+	uint16_t y1;
+	uint16_t y2;
+}line_t;
+
+line_t line_arr[60];
+//----------------------------------------------
+uint8_t rom_blue[8] = {0x28, 0xc5, 0xc4, 0x7, 0xd6, 0x1, 0x3c, 0xc0};
+uint8_t rom_black[8] = {0x28, 0x3b, 0xe5, 0x7, 0xd6, 0x1, 0x3c, 0x94};
+
+//---------------------------------------------
+
+
+void app_main(void)
+{
+	//off fucking stereo
+	gpio_set_direction(25, GPIO_MODE_OUTPUT);
+	gpio_set_level(25, 0);
+	//Intro animation
+	//Initialization
+	 /* uint16_t i,j;
+	  esp_err_t ret;
+
+	  // Configure SPI bus
+	  spi_bus_config_t cfg = {
+	      .mosi_io_num = CONFIG_PIN_NUM_MOSI,
+	      .miso_io_num = -1,
+	      .sclk_io_num = CONFIG_PIN_NUM_CLK,
+	      .quadwp_io_num = -1,
+	      .quadhd_io_num = -1,
+	      .max_transfer_sz = 16*320*2+8,
+	  };
+	  ret = spi_bus_initialize(HSPI_HOST, &cfg, SPI_DMA_CH_AUTO);
+	  //ESP_LOGI(TAG, "spi bus initialize: %d", ret);
+	  spi_device_interface_config_t devcfg={
+	        .clock_speed_hz=10*1000*1000,           //Clock out at 10 MHz
+	        .mode=0,                                //SPI mode 0
+	        .spics_io_num=CONFIG_PIN_NUM_CS,               //CS pin
+	        .queue_size=7,                          //We want to be able to queue 7 transactions at a time
+	        .pre_cb=lcd_spi_pre_transfer_callback,  //Specify pre-transfer callback to handle D/C line
+	  };
+	  ret=spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
+	  //ESP_LOGI(TAG, "spi bus add device: %d", ret);
+	  TFT9341_ini(spi, 320, 240);
+	  TFT9341_FillScreen(spi, TFT9341_YELLOW);
+	  TFT9341_DrawLine(spi, TFT9341_CYAN, 10, 30, 100, 200);
+	  for(int i = 150; i < 250; i++){
+		  for(int j = 100; j < 200; j++){
+			  TFT9341_DrawPixel(spi, j, i, TFT9341_MAGENTA);
+		  }
+	  }*/
+	  //TFT9341_SetRotation(spi, 0);
+	//uint16_t a = 15;
+	line_arr[0].x1 = 15;
+	for(int i = 0; i < 60; i++){
+		line_arr[i].x2 = (line_arr[i].x1 + 5);
+		line_arr[i + 1].x1 = line_arr[i].x2;
+	}
+	ESP_LOGI(TAG, "%d %d %d %d %d %d ", line_arr[0].x1, line_arr[0].x2, line_arr[1].x1,
+			line_arr[1].x2, line_arr[59].x1, line_arr[59].x2);
+	TFT9341_spi_init(); // Init LCD and SPI
+	ds18b20_init(TEMP_BUS);
+	ds18b20_reset();
+	ds18b20_write_byte(SKIP_ROM);
+	ds18b20_write_byte(WRITE_SCRATCH);
+	ds18b20_write_byte(33); // high alarm temp
+	ds18b20_write_byte(22); // low alarm temp
+	ds18b20_write_byte(TEMP_12_BIT);
+	ds18b20_reset();
+
+	//ds18b20_readScratchPad(SKIP_ROM, 1, dt);
+	//Visualization
+	//Check if OK
+	//Error task
+	//Startup
+	//Measurements+
+
+	//PID output
+	  TFT9341_FillScreen(spi, TFT9341_YELLOW);
+	  TFT9341_DrawLine(spi, TFT9341_CYAN, 10, 30, 100, 200);
+	  for(int i = 150; i < 250; i++){
+		  for(int j = 100; j < 200; j++){
+			  TFT9341_DrawPixel(spi, j, i, TFT9341_MAGENTA);
+		  }
+	  }
+	TFT9341_FillScreen(spi, TFT9341_MAGENTA);
+	ds18b20_reset();
+	ds18b20_write_byte(READ_ROM);
+	for(int i = 0; i < 8; i++){
+		rom[i] = ds18b20_read_byte();
+	}
+
+	ESP_LOGI(TAG, "ROM: %x %x %x %x %x %x %x %x ",
+			rom[0], rom[1], rom[2], rom[3], rom[4], rom[5], rom[6], rom[7]);
+	vTaskDelay(1000 / portTICK_PERIOD_MS);
+    while (true) {
+
+    	ds18b20_requestTemperatures();
+    	cTemp = ds18b20_get_temp(MATCH_ROM, rom_black);
+
+    	TFT9341_SetTextColor(TFT9341_YELLOW);
+    	TFT9341_SetBackColor(TFT9341_BLUE);
+    	TFT9341_SetFont(&Font24);
+
+    	sprintf(str, "Esp32");
+    	TFT9341_String(spi, 10, 10, str);
+    	sprintf(str, "Temp blk:%0.2fC", cTemp);
+    	TFT9341_String(spi, 5, 35, str);
+    	cTemp = ds18b20_get_temp(MATCH_ROM, rom_blue);
+    	sprintf(str, "Temp blu:%0.2fC", cTemp);
+    	TFT9341_String(spi, 5, 70, str);
+    	vTaskDelay(10 / portTICK_PERIOD_MS);
+
+    }
+}
+
+void lcd_spi_pre_transfer_callback(spi_transaction_t *t)
+{
+    int dc=(int)t->user;
+    gpio_set_level(CONFIG_PIN_NUM_DC, dc);
+}
+
+void TFT9341_spi_init(void)
+{
+	  uint16_t i,j;
+	  esp_err_t ret;
+
+	  // Configure SPI bus
+	  spi_bus_config_t cfg = {
+	      .mosi_io_num = CONFIG_PIN_NUM_MOSI,
+	      .miso_io_num = -1,
+	      .sclk_io_num = CONFIG_PIN_NUM_CLK,
+	      .quadwp_io_num = -1,
+	      .quadhd_io_num = -1,
+	      .max_transfer_sz = 16*320*2+8,
+	  };
+	  ret = spi_bus_initialize(HSPI_HOST, &cfg, SPI_DMA_CH_AUTO);
+	  //ESP_LOGI(TAG, "spi bus initialize: %d", ret);
+	  spi_device_interface_config_t devcfg={
+	        .clock_speed_hz=10*1000*1000,           //Clock out at 10 MHz
+	        .mode=0,                                //SPI mode 0
+	        .spics_io_num=CONFIG_PIN_NUM_CS,               //CS pin
+	        .queue_size=7,                          //We want to be able to queue 7 transactions at a time
+	        .pre_cb=lcd_spi_pre_transfer_callback,  //Specify pre-transfer callback to handle D/C line
+	  };
+	  ret=spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
+	  //ESP_LOGI(TAG, "spi bus add device: %d", ret);
+	  TFT9341_ini(spi, 320, 240);
+	  //TFT9341_SetRotation(spi, 0);
+
+}
